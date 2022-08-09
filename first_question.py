93c6f18dbf43a99f8a4e83c3f1744a661b7c6110
dolby360 <dolevben054@gmail.com>
2022-08-09 22:10:20 +0300

    make some process, still not sure about the architecure

--- first_question.py
+++ first_question.py
@@ -2,9 +2,21 @@ import json
 import pathlib
 import os
 from prompt_handler import show_prompt_for_questions
-CURR_DIR = pathlib.Path(__file__).resolve().parent
-FIRST_QUESTION_PATH = os.path.join(CURR_DIR, "questions", "first_question.json")
+from enum import Enum
 
-def ask_first_question():
-    with open(FIRST_QUESTION_PATH, 'r') as first_q:
-        show_prompt_for_questions(json.load(first_q))
+class firstStates(Enum):
+    DISCOVER = "Discover new switcher devices"
+    KNOWN_DEVICE = "I want to make something with known device"
+
+data = [
+    {
+        "type": "list",
+        "name": "action",
+        "message": "What action do you want to perform?",
+        "choices": [firstStates.DISCOVER.value, firstStates.KNOWN_DEVICE.value]
+    }
+]
+
+def ask_first_question() -> dict:
+    return show_prompt_for_questions(data)
+        
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from first_question import ask_first_question
 
-def get_next_state(answare):
-    pass
+class stateGen:
+    def __init__(self) -> None:
+        self.state_list = []
+
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
 
 def main():
-    ans = ask_first_question()
-    print(ans)
+    state_manager = stateGen()
+    for state in state_manager:
+        state()
         
 if __name__ == "__main__":
     main()
